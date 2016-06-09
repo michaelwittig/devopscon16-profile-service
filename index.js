429633ff91835a4213b4516d271a64efb70c9aff
Michael Wittig <post@michaelwittig.info>
2016-06-09 17:14:14 +0200

    implemented dumy lambda and added api gateway

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 
 exports.handler = function(event, context, cb) {
   console.log(JSON.stringify(event));
-  cb({});
+  cb({
+    "id": event.id,
+    "label": "test",
+    "location": {
+      "latitude": 0,
+      "longitude": 0
+    }
+  });
 };
