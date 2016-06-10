098440e919893b945454e12b7a79714ed0d7c1a6
Michael Wittig <post@michaelwittig.info>
2016-06-10 07:46:10 +0200

    fixed response data

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 exports.handler = function(event, context, cb) {
   console.log(JSON.stringify(event));
   cb({
-    "id": event.id,
-    "label": "test",
-    "location": {
-      "latitude": 0,
-      "longitude": 0
+    "body": {
+      "id": event.id,
+      "label": "test",
+      "location": {
+        "latitude": 0,
+        "longitude": 0
+      }
     }
   });
 };
