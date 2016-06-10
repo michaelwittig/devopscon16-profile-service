dadef2896cdcb9c833172321148b9385c426c798
Michael Wittig <post@michaelwittig.info>
2016-06-10 15:37:35 +0200

    integrated location service

---
+++ README.md
@@ -0,0 +1,1 @@
+curl -vvv "https://onvb9sq5yc.execute-api.us-east-1.amazonaws.com/v1/profile/123"
--- index.js
+++ index.js
@@ -1,37 +1,54 @@
 "use strict";
 
+var config = require("./config.json");
 var AWS = require("aws-sdk");
 var dynamodb = new AWS.DynamoDB();
+var lambda = new AWS.Lambda();
 
 exports.handler = function(event, context, cb) {
   console.log(JSON.stringify(event));
-  var id = event.id;
-  dynamodb.getItem({
-    "Key": {
-      "id": {
-        "S": id
-      }
-    },
-    "TableName": "profile"
+  lambda.invoke({
+    "FunctionName": config.LocationLambdaArn,
+    "InvocationType": "RequestResponse",
+    "Payload": JSON.stringify({"id": event.id, "action": "get"})
   }, function(err, data) {
     if (err) {
       cb(err);
     } else {
-      if (data.Item === undefined) {
-        cb(new Error("ServiceErrorNotFound"));
-      } else {
-        console.log(JSON.stringify(data.Item));
-        cb(null, {
-          "status": 200,
-          "body": {
-            "id": data.Item.id.S,
-            "label": data.Item.label.S,
-            "location": {
-              "latitude": data.Item.latitude.N,
-              "longitude": data.Item.longitude.N
+      console.log(JSON.stringify(data));
+      if (data.FunctionError === undefined) {
+        var location = JSON.parse(data.Payload);
+        dynamodb.getItem({
+          "Key": {
+            "id": {
+              "S": event.id
+            }
+          },
+          "TableName": "profile"
+        }, function(err, data) {
+          if (err) {
+            cb(err);
+          } else {
+            if (data.Item === undefined) {
+              cb(new Error("ServiceErrorNotFound"));
+            } else {
+              console.log(JSON.stringify(data.Item));
+              cb(null, {
+                "status": 200,
+                "body": {
+                  "id": data.Item.id.S,
+                  "label": data.Item.label.S,
+                  "location": {
+                    "latitude": location.body.latitude,
+                    "longitude": location.body.longitude
+                  }
+                }
+              });
             }
           }
         });
+      } else {
+        cb(new Error(data.FunctionError + ": " + data.Payload));
       }
     }
   });
