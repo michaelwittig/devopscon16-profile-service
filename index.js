9366bd3970385da16f03b635bcd6a73cc1ce816d
Michael Wittig <post@michaelwittig.info>
2016-06-14 08:17:44 +0200

    improved errors

--- index.js
+++ index.js
@@ -7,32 +7,33 @@ var lambda = new AWS.Lambda();
 
 exports.handler = function(event, context, cb) {
   console.log(JSON.stringify(event));
-  lambda.invoke({
-    "FunctionName": config.LocationLambdaArn,
-    "InvocationType": "RequestResponse",
-    "Payload": JSON.stringify({"id": event.id, "action": "get"})
+  
+  dynamodb.getItem({
+    "Key": {
+      "id": {
+        "S": event.id
+      }
+    },
+    "TableName": "profile"
   }, function(err, data) {
     if (err) {
       cb(err);
     } else {
       console.log(JSON.stringify(data));
-      if (data.FunctionError === undefined) {
-        var location = JSON.parse(data.Payload);
-        dynamodb.getItem({
-          "Key": {
-            "id": {
-              "S": event.id
-            }
-          },
-          "TableName": "profile"
+      if (data.Item === undefined) {
+        cb(new Error("[NotFound] Profile not found"));
+      } else {
+        lambda.invoke({
+          "FunctionName": config.LocationLambdaArn,
+          "InvocationType": "RequestResponse",
+          "Payload": JSON.stringify({"id": event.id, "action": "get"})
         }, function(err, data) {
           if (err) {
             cb(err);
           } else {
             console.log(JSON.stringify(data));
-            if (data.Item === undefined) {
-              cb(new Error("ServiceErrorNotFound"));
-            } else {
+            if (data.FunctionError === undefined) {
+              var location = JSON.parse(data.Payload);
               cb(null, {
                 "status": 200,
                 "body": {
@@ -44,11 +45,11 @@ exports.handler = function(event, context, cb) {
                   }
                 }
               });
+            } else {
+              cb(new Error("[InternalServerError] " + data.FunctionError + ": " + data.Payload));
             }
           }
         });
-      } else {
-        cb(new Error(data.FunctionError + ": " + data.Payload));
       }
     }
   });
